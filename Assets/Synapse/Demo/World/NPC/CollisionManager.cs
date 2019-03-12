d42fb2be7544a3c68a40902034be77ff2450e54d
pointerrrr <gideonogilvie@gmail.com>
2019-03-12 20:56:15 +0100

    initial upload

---
+++ Assets/Synapse/Demo/World/NPC/CollisionManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Synapse
+{
+	namespace Demo
+	{
+		[AddComponentMenu("Synapse/Demo/NPC/Collision Manager")]
+		public class CollisionManager : MonoBehaviour
+		{
+			void OnControllerColliderHit(ControllerColliderHit a_collision)
+			{
+				CollectibleValue collectible = a_collision.collider.GetComponent<CollectibleValue>();
+					
+				if(collectible != null  &&  CollectibleValue.m_instances.Contains(collectible))
+				{
+					Points componentPoints = GetComponent<Points>();
+					componentPoints.Add(collectible.Points);
+					
+					CollectibleValue.m_instances.Remove(collectible);
+					Destroy(collectible.gameObject);
+
+                    GameObject thePlayer = GameObject.Find("ThePlayer");
+                    SynapseAgent synapse = GetComponent<SynapseAgent>();
+                    synapse.health = 100;
+                    synapse.counter = 0;
+
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Synapse/Demo/World/NPC/Heat.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Synapse
+{
+	namespace Demo
+	{
+		[AddComponentMenu("Synapse/Demo/NPC/Heat")]
+		public class Heat : MonoBehaviour
+		{
+			public Gradient m_gradient;
+			public float m_minimalMoveToHeat;
+			public float m_heatSpeed;
+			public float m_coldSpeed;
+			
+			private Renderer m_renderer;
+			private Vector3 m_lastPosition;
+			private float m_heatValue;
+			
+			void Start()
+			{
+				m_heatValue = 0.0f;
+				m_renderer = GetComponentInChildren<Renderer>();
+				m_lastPosition = transform.position;
+				
+				ApplyColor();
+			}
+			
+			void Update()
+			{
+				Vector3 deltaMove = transform.position - m_lastPosition;
+				if(deltaMove.sqrMagnitude >= m_minimalMoveToHeat * m_minimalMoveToHeat * Time.deltaTime)
+				{
+                    m_heatValue = 0;
+				}
+				else
+				{
+                    m_heatValue = 0;
+				}
+				m_lastPosition = transform.position;
+				ApplyColor();
+			}
+			
+			void ApplyColor()
+			{
+				Color color = m_gradient.Evaluate(m_heatValue);
+				m_renderer.material.color = color;
+			}
+			
+			public float HeatValue{ get{ return m_heatValue; } }
+		}
+	}
+}
